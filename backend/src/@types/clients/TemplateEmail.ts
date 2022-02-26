a2dfa41a020d3b367b2ed339c2fe526e51008955
Joao <joaotst@hotmail.com>
2022-02-26 20:54:04 -0300

    bug fix

--- backend/src/@types/clients/TemplateEmail.ts
+++ backend/src/@types/clients/TemplateEmail.ts
@@ -7,6 +7,7 @@ export type Message = {
 
 export const TemmplateEmail = `
     <h1> Olá, CLIENTE. </h1>
-    <h2> Aqui está seu orçamento, no valor de R$ VALOR </h2>
+    <h2> Aqui está seu orçamento, no valor de ${preco} </h2>
     <p> Por favor, verifique se o orçamento em anexo contempla os doces solicitados para confirmação do pedido. </p> </br>
-    <p> Lembramos que a confirmação do pedido deve ser realizada até 48h antes do evento </p>`
+    <p> Lembramos que a confirmação do pedido deve ser realizada até 48h antes do evento </p>
+`
